4f7d5dc35547b97aee9d593a9f60b96d06ec93b3
simon.sammi <simon.sammi@gmail.com>
2014-10-28 13:55:26 +0800

    merge two small files

--- web/game/utils.go
+++ web/game/utils.go
@@ -1,12 +1,16 @@
 package game
 
 import (
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"github.com/gorilla/securecookie"
 	"hash/fnv"
 )
 
 const (
 	REDIS_HASH_MAX = 1000
+	SESSION_ID     = "SID"
 )
 
 func Hash(s string) uint64 {
@@ -17,4 +21,12 @@ func Hash(s string) uint64 {
 
 func RedisHashKey(prefix, value string) string {
 	return fmt.Sprint(prefix, ":", Hash(value)/REDIS_HASH_MAX)
+}
+
+func RandomString() (string, error) {
+	if key := securecookie.GenerateRandomKey(128); key == nil {
+		return "", errors.New("generate random key failed")
+	} else {
+		return base64.StdEncoding.EncodeToString(key), nil
+	}
 }
