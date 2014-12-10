c037003175614e901c46d390d4857d710ab138be
simon.sammi <simon.sammi@gmail.com>
2014-12-10 18:37:57 +0800

    refactoring, remove the duplication and useless code

--- web/spin.go
+++ web/spin.go
@@ -2,31 +2,16 @@ package web
 
 import (
 	"errors"
-	"fmt"
 	"github.com/fzzy/radix/redis"
 	"github.com/gorilla/mux"
 	"github.com/landjur/go-decimal"
 	"log"
 	"net/http"
+	"sslot/engine"
 	"sslot/games"
 	"strconv"
 	"time"
 )
-
-func UserHash(username string) string {
-	return fmt.Sprint("user:", username)
-}
-
-func GameFieldLines(gamename string) string {
-	return fmt.Sprint("game_", gamename, "_lines")
-}
-func GameFieldBet(gamename string) string {
-	return fmt.Sprint("game_", gamename, "_bet")
-}
-
-func GameFieldFeatures(gamename string) string {
-	return fmt.Sprint("game_", gamename, "_features")
-}
 
 func ShowGame(w http.ResponseWriter, r *http.Request) {
 	// if game name is not valid, return directly
@@ -67,6 +52,20 @@ func ShowGame(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func GetUserNameFromRedis(r *http.Request) (string, error) {
+	conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(2)*time.Second)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	username, _, err := GetUserName(conn, r)
+	if err != nil {
+		return "", err
+	}
+	return username, nil
+}
+
 func GetUserName(conn *redis.Client, r *http.Request) (string, bool, error) {
 	sid, hash := AuthHash(r)
 	reply := conn.Cmd("HGET", hash, sid)
@@ -83,102 +82,105 @@ func GetUserName(conn *redis.Client, r *http.Request) (string, bool, error) {
 	return username, false, nil
 }
 
-func FreeSpinGame(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	gamename := vars["game"]
-	if !games.ShowGame(gamename) {
-		http.NotFound(w, r)
-		return
-	}
-	// check if user authenticated
-	conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(2)*time.Second)
-	if err != nil {
-		log.Println("connect to redis error:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer conn.Close()
-
-	// //if user authed, then get the username, otherwise use session id as username
-	username, _, err := GetUserName(conn, r)
-	if err != nil {
-		log.Println("get user name from redis error:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	result, err := games.PlayerFreeSpin(gamename, username)
-	if err != nil {
-		log.Println("player free spin error:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	writeJson(w, r, result)
+func NormalSpinGame(w http.ResponseWriter, r *http.Request) {
+	sp, err := validateNormalSpinReq(r)
+	SpinGame(w, r, sp, err)
 }
 
-type SpinParams struct {
+func FreeSpinGame(w http.ResponseWriter, r *http.Request) {
+	sp, err := validateFreeSpinReq(r)
+	SpinGame(w, r, sp, err)
+}
+
+type SpinAction interface {
+	gameName() string
+	spin(username string) (*engine.SpinResult, error)
+}
+
+type FreeSpinParams struct {
+	game string
+}
+
+func (sp FreeSpinParams) gameName() string {
+	return sp.game
+}
+
+func (sp FreeSpinParams) spin(username string) (*engine.SpinResult, error) {
+	return games.PlayerFreeSpin(sp.game, username)
+}
+
+type NormalSpinParams struct {
 	game  string
 	lines int
 	bet   *decimal.Decimal
 }
 
-func validateRequest(r *http.Request) (*SpinParams, error) {
+func (sp NormalSpinParams) gameName() string {
+	return sp.game
+}
+
+func (sp NormalSpinParams) spin(username string) (*engine.SpinResult, error) {
+	return games.PlayerMainSpin(sp.game, username, sp.lines, sp.bet)
+}
+
+func validateFreeSpinReq(r *http.Request) (*FreeSpinParams, error) {
 	vars := mux.Vars(r)
 	game, ok := vars["game"]
 	if !ok {
 		return nil, errors.New("Param game required")
 	}
+	return &FreeSpinParams{game}, nil
+}
+
+func validateNormalSpinReq(r *http.Request) (*NormalSpinParams, error) {
+	vars := mux.Vars(r)
+
+	game, ok := vars["game"]
+	if !ok {
+		return nil, errors.New("Param game required")
+	}
+
 	sLines, ok := vars["lines"]
 	if !ok {
 		return nil, errors.New("Param lines required")
 	}
+
 	lines, err := strconv.Atoi(sLines)
 	if err != nil {
 		return nil, errors.New("Param lines is not number")
 	}
+
 	sBet, ok := vars["bet"]
 	if !ok {
 		return nil, errors.New("Param bet required")
 	}
+
 	bet, err := decimal.Parse(sBet)
 	if err != nil {
 		return nil, errors.New("Param bet is not decimal")
 	}
-	return &SpinParams{game, lines, bet}, nil
+
+	return &NormalSpinParams{game, lines, bet}, nil
 }
 
-func NormalSpinGame(w http.ResponseWriter, r *http.Request) {
-	//if game name given not right, return directly
-	sp, err := validateRequest(r)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-	gamename, newlines, newbet := sp.game, sp.lines, sp.bet
-
-	if !games.ShowGame(gamename) {
+func SpinGame(w http.ResponseWriter, r *http.Request, sp SpinAction, valiateErr error) {
+	if valiateErr != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	// check if user authenticated
-	conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(2)*time.Second)
-	if err != nil {
-		log.Println("connect to redis error:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if !games.ShowGame(sp.gameName()) {
+		http.NotFound(w, r)
 		return
 	}
-	defer conn.Close()
 
-	// //if user authed, then get the username, otherwise use session id as username
-	username, _, err := GetUserName(conn, r)
+	username, err := GetUserNameFromRedis(r)
 	if err != nil {
-		log.Println("get user name from redis error:", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if result, err := games.PlayerMainSpin(gamename, username, newlines, newbet); err != nil {
+	if result, err := sp.spin(username); err != nil {
 		log.Println("player main spin error:", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
